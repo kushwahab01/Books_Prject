5abf911c6209084f4d6ed89d9e4a73c4e226e69e
Cybermintin <Cybermintin@DESKTOP-PJMDP27>
2022-12-06 15:52:53 +0530

    Add project files.

---
+++ Books_Prject/Books.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books_Prject
+{
+    class Books
+    {
+        private int id;
+        private string name;
+        private string author;
+        private double price;
+        public Books(int id, string name, string author, double price)
+        {
+            this.id = id;
+            this.name = name;
+            this.author = author;
+            this.price = price;
+        }
+        public override string ToString()
+        {
+            return $"Book id :{id} , book name : {name} , book author : {author} , Book price : {price}";
+        }
+        static void Main(string[] args)
+        {
+            Books b = new Books(101, "Wing", "Harry", 210);
+            Console.WriteLine(b);
+        }
+    
+}
+}
---
+++ Books_Prject/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books_Prject
+{
+    public class Employee
+    {
+        private string name;
+        private int id;
+        private double salary;
+        private static  int count;
+        private string country;
+        public Employee(string name, double salary, string country="India")
+        {
+            count++;
+            id = count;
+            this.name = name;
+            this.salary = salary;
+            this.country = country;
+
+        }
+        public override string ToString()
+        {
+            return $"{name}-->{id}-->{salary}-->{country}";
+        }
+        static void Main(string[] args)
+        {
+            Employee e = new Employee("Brijesh", 245600.0);
+            Employee e1 = new Employee("Sanjay", 25000.15);
+            Employee e2 = new Employee("Abhishek", 354600.05);
+
+            Console.WriteLine(e);
+            Console.WriteLine(e1);
+            Console.WriteLine(e2);
+        }
+    }
+}
