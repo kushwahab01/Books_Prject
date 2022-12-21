315771360fbd7435931bb68f80008101de709d77
kushwahab01 <b.r.kushwaha8796@gmail.com>
2022-12-21 09:21:08 +0530

    push repository

---
+++ Books_Prject/AlternateNumber.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Books_Prject
+{
+    class AlternateNumber
+    {
+       
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the size");
+            int size = int.Parse(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("Enter an element");
+            for(int i=0; i<arr.Length; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+            
+            for(int i=0; i<arr.Length; i+=2)
+            {
+                Console.WriteLine("Alternate array element is: "+arr[i]);
+            }
+            
+
+        }
+    }
+}
